f72149a685b655651c147f19d0c213e838c5079e
Eddie Wilkinson <114743507+Edmentos@users.noreply.github.com>
2025-11-10 17:25:12 +0000

    going to add tests next these are all the endpojnds and schemas

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,2 +1,9 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
+from app.main import app, get_db
+from app.models import Base
 
