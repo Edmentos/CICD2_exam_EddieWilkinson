31284b821257bea6390b965f545ec3d81419ce07
Eddie Wilkinson <114743507+Edmentos@users.noreply.github.com>
2025-11-10 16:16:04 +0000

    added models

--- app/models.py
+++ app/models.py
@@ -4,4 +4,21 @@ from sqlalchemy import String, Integer, ForeignKey, UniqueConstraint
 class Base(DeclarativeBase):
     pass
 
+class AuthorDB(Base):
+    __tablename__ = "Authors"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]=mapped_column(String(100), nullable = False)
+    email: Mapped[str]=mapped_column(Unique = True, nullable = False)
+    year_started: Mapped[int]=mapped_column(Integer, nullable = False)
+
+
+class BookDB(Base):
+    __tablename__ = "Books"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]=mapped_column(String(255), nullable = False)
+    pages: Mapped[str]=mapped_column(String(10000), nullable = False)
+    authors_id Mapped[int]=mapped_column(ForeignKey("authors.id",ondelete="Cascade"))
+
+    owner:Mapped["AuthorDB"] = relationship(back_populates"Books")
